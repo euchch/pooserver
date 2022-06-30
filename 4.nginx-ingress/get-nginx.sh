3316c375ec9a14c80ba113f6b75e7304d604f21c
Yaakov Yehezkel <YehezkelY@aetna.com>
2022-06-30 12:37:54 -0400

    Requesting nginx-controller-ingress help :/

--- 4.nginx-ingress/get-nginx.sh
+++ 4.nginx-ingress/get-nginx.sh
@@ -4,4 +4,4 @@ helm repo update
 
 # Install metallb
 kubectl create namespace nginx
-helm install poonginx nginx-stable/nginx-ingress -n nginx --set controller.service.loadBalancerIP=192.168.1.254,rbac.create=true
+helm install poonginx nginx-stable/nginx-ingress -n nginx -f values.yaml
